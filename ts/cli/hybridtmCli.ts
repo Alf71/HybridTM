3088c508d15ce948f22100fdea1a5ed618dd2488
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-07-24 10:20:41 -0300

    Fixed license in headers

--- ts/cli/hybridtmCli.ts
+++ ts/cli/hybridtmCli.ts
@@ -3,7 +3,7 @@
  * Copyright (c) 2025-2026 Maxprograms.
  *
  * This program and the accompanying materials
- * are made available under the terms of the Eclipse  License 1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/org/documents/epl-v10.html
  *
